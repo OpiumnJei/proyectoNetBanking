36355d3b0be6f9574841da3cda37d798f4871d77
OpiumnJei <98902680+OpiumnJei@users.noreply.github.com>
2025-01-11 12:50:17 -0400

    Bcrypt config agregada

---
+++ src/main/java/proyectoNetBanking/infra/security/BcryptBeanConfig.java
@@ -0,0 +1,16 @@
+package proyectoNetBanking.infra.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+//CONFIGURACION PARA LA ENCRIPTACION DE LAS CONTRASENIAS
+//hay que crear esta configuracion manualmente ya que spring no la genera automaticamente
+@Configuration
+public class BcryptBeanConfig {
+
+    @Bean // un bean representa una pieza fundamental de tu lógica de negocio o configuración.
+    public BCryptPasswordEncoder passwordEncoder(){
+       return new BCryptPasswordEncoder();
+    }
+}
