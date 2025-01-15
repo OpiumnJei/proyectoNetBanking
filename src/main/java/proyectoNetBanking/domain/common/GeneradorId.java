270a711adacc4836fc3a35e45b7d1a3cc2882082
OpiumnJei <98902680+OpiumnJei@users.noreply.github.com>
2025-01-14 22:19:20 -0400

    la clase para generar el id de los productos ha sido refactorizada, agregando una interfaz funcional del tipo Predicate

--- src/main/java/proyectoNetBanking/domain/common/GeneradorId.java
+++ src/main/java/proyectoNetBanking/domain/common/GeneradorId.java
@@ -3,20 +3,28 @@ package proyectoNetBanking.domain.common;
 import org.springframework.stereotype.Component;
 
 import java.security.SecureRandom;
+import java.util.function.Predicate;
 
 @Component
-public class GeneradorId {
+public class GeneradorId{
 
     //metodo para generar el identificador unico del producto
-    public String generarIdProducto() {
-        String idProducto;
+    private String generarIdProducto() {
 
         /*
         * SecureRandom es una clase en Java utilizada para generar números aleatorios de alta calidad criptográfica.
-        *
         * */
         // Generar número como cadena
-        idProducto = String.valueOf(100_000_000 + new SecureRandom().nextInt(900_000_000));
-        return idProducto;
+        return String.valueOf(100_000_000 + new SecureRandom().nextInt(900_000_000));
+    }
+
+    //generar id del producto y verificar que no exista un producto con ese id
+    public String generarIdUnicoProducto(Predicate<String> verificarId) {
+        String idGenerado;
+        do {
+            idGenerado = generarIdProducto();//id generado
+        }
+        while (verificarId.test(idGenerado)); //validar que el id del producto generado no exista en la bd
+        return idGenerado;
     }
 }
