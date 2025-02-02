392daba5db6345e0c172e9ab41944c053b8cb56b
OpiumnJei <98902680+OpiumnJei@users.noreply.github.com>
2025-02-02 18:14:47 -0400

    indicadores service  para el dashboard del usuario del tipo admininistrador agregados

--- src/main/java/proyectoNetBanking/domain/prestamos/PrestamoRepository.java
+++ src/main/java/proyectoNetBanking/domain/prestamos/PrestamoRepository.java
@@ -1,6 +1,7 @@
 package proyectoNetBanking.domain.prestamos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import proyectoNetBanking.domain.productos.EstadoProducto;
 
 import java.util.List;
 
@@ -12,4 +13,5 @@ public interface PrestamoRepository extends JpaRepository<Prestamo, Long> {
 
     //se hace un conteo en la bd de los prestamos activos, es decir que sean igual a 1
     int countByEstadoProductoId(Long idEstado);
+    Long countByEstadoProductoId(EstadoProducto estadoProducto);
 }
