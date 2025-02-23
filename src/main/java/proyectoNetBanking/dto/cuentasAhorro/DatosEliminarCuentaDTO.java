0502badbf3f2e8553c4041e513028037886d01c8
OpiumnJei <98902680+OpiumnJei@users.noreply.github.com>
2025-02-23 16:46:07 -0400

    cambio en el nombre de los campos del record

--- src/main/java/proyectoNetBanking/dto/cuentasAhorro/DatosEliminarCuentaDTO.java
+++ src/main/java/proyectoNetBanking/dto/cuentasAhorro/DatosEliminarCuentaDTO.java
@@ -5,8 +5,8 @@ import jakarta.validation.constraints.NotNull;
 //Datos enviados por el cliente para la eliminacion de cuentas
 public record DatosEliminarCuentaDTO(
         @NotNull(message = "El id del usuario no puede estar vacio.")
-        Long idUsuario,
+        Long usuarioId,
         @NotNull(message = "El id de la cuenta no puede estar vacio.")
-        Long idCuenta
+        Long cuentaId
 ) {
 }
