53b19528460bce8bf89dcdc8d822b7ea3cd8a95b
OpiumnJei <98902680+OpiumnJei@users.noreply.github.com>
2025-02-23 17:38:51 -0400

    Retorno de respuesta al cliente y logica faltante para la correcta creacion de una tarjeta de credito agregada

--- src/main/java/proyectoNetBanking/service/tarjetasCredito/TarjetaCreditoService.java
+++ src/main/java/proyectoNetBanking/service/tarjetasCredito/TarjetaCreditoService.java
@@ -3,10 +3,16 @@ package proyectoNetBanking.service.tarjetasCredito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import proyectoNetBanking.domain.common.GeneradorId;
+import proyectoNetBanking.domain.productos.EstadoProducto;
+import proyectoNetBanking.domain.productos.EstadoProductoEnum;
+import proyectoNetBanking.dto.tarjetasCredito.TarjetaCreditoResponseDTO;
 import proyectoNetBanking.infra.errors.DatosInvalidosException;
+import proyectoNetBanking.infra.errors.UsuarioInactivoException;
 import proyectoNetBanking.repository.CuentaAhorroRepository;
 import proyectoNetBanking.dto.tarjetasCredito.DatosTarjetaDTO;
 import proyectoNetBanking.domain.tarjetasCredito.TarjetaCredito;
+import proyectoNetBanking.repository.EstadoProductoRepository;
 import proyectoNetBanking.repository.TarjetaRepository;
 import proyectoNetBanking.domain.usuarios.Usuario;
 import proyectoNetBanking.repository.UsuarioRepository;
@@ -29,8 +35,14 @@ public class TarjetaCreditoService {
     @Autowired
     private CuentaAhorroRepository cuentaAhorroRepository;
 
+    @Autowired
+    private GeneradorId generadorId; //inyectamos el generador de ids
+
+    @Autowired
+    private EstadoProductoRepository estadoProductoRepository;
+
     @Transactional
-    public void crearTarjetaCredito(Long usuarioId, DatosTarjetaDTO datosTarjetaDTO) {
+    public TarjetaCreditoResponseDTO crearTarjetaCredito(Long usuarioId, DatosTarjetaDTO datosTarjetaDTO) {
 
         if (usuarioId == null || usuarioId <= 0) { //validar que numero no sea negativo ni nulo
             throw new DatosInvalidosException("El ID del usuario no puede ser nulo ni un número negativo");
@@ -40,23 +52,49 @@ public class TarjetaCreditoService {
         Usuario usuario = usuarioRepository.findById(usuarioId)
                 .orElseThrow(() -> new UsuarioNotFoundException());
 
+        if (!usuario.isActivo()) {
+            throw new UsuarioInactivoException("El usuario introducido se encuentra inactivo");
+        }
         //validar que el limite de credito sea mayor o igual 2000
         if (datosTarjetaDTO.limiteCredito().compareTo(MONTO_MINIMO_LIMITE_CREDITO) < 0) {
             throw new RuntimeException("El limite de credito introducido es menor al monto minimo aceptado.");
         }
 
-        asignarTarjetaCredito(usuario, datosTarjetaDTO.limiteCredito());
+        TarjetaCredito tarjetaCreada = asignarTarjetaCredito(usuario, datosTarjetaDTO.limiteCredito());
+
+        return new TarjetaCreditoResponseDTO(
+                tarjetaCreada.getUsuario().getId(),
+                tarjetaCreada.getIdProducto(),
+                tarjetaCreada.getLimiteCredito(),
+                tarjetaCreada.getCreditoDisponible(),
+                tarjetaCreada.getCreated()
+        );
+
     }
 
-    private void asignarTarjetaCredito(Usuario usuario, BigDecimal limiteCredito) {
+    private TarjetaCredito asignarTarjetaCredito(Usuario usuario, BigDecimal limiteCredito) {
 
         TarjetaCredito tarjetaUsuario = new TarjetaCredito();
+        tarjetaUsuario.setIdProducto(generarIdUnicoProducto());
         tarjetaUsuario.setLimiteCredito(limiteCredito);
         tarjetaUsuario.setCreditoDisponible(limiteCredito);
+        tarjetaUsuario.setSaldoPorPagar(BigDecimal.ZERO); //al crear una nueva tarjeta el saldo por pagar debe ser 0
         tarjetaUsuario.setUsuario(usuario);
-        tarjetaRepository.save(tarjetaUsuario);
+        tarjetaUsuario.setEstadoProducto(colocarEstadoProductos(EstadoProductoEnum.ACTIVO.name()));
+        return tarjetaRepository.save(tarjetaUsuario);
     }
 
+    private EstadoProducto colocarEstadoProductos(String nombreEstado) {
+
+        return estadoProductoRepository.findByNombreEstadoIgnoreCase(nombreEstado)
+                .orElseThrow(() -> new RuntimeException("El estado no existe"));
+    }
+
+    //generar id del producto
+    private String generarIdUnicoProducto() {
+
+        return generadorId.generarIdUnicoProducto(cuentaAhorroRepository::existsByIdProducto); //se traduce del repositorio toma el metodo existsbyIdProducto como una referencia
+    }
 
 
 }
