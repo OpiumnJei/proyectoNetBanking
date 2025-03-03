2ea879635d2e643a83ed83e6b8bc787f3201332b
OpiumnJei <98902680+OpiumnJei@users.noreply.github.com>
2025-03-02 20:15:02 -0400

    cambio de nombre de algunos campos

--- src/main/java/proyectoNetBanking/dto/pagos/DatosPagoExpresoDTO.java
+++ src/main/java/proyectoNetBanking/dto/pagos/DatosPagoExpresoDTO.java
@@ -5,16 +5,16 @@ import jakarta.validation.constraints.*;
 import java.math.BigDecimal;
 
 public record DatosPagoExpresoDTO(
+        @NotNull(message = "El id de la cuenta de ahorro es requerido")
+        Long cuentaOrigenId,
 
         @NotBlank(message = "El numero de cuenta es un campo requerido")
         @Pattern(regexp = "\\d{9}", message = "El numero de cuenta debe contener 9 dígitos numéricos.")
-        String numeroCuenta,
-
-        @NotNull(message = "El id de la cuenta de ahorro es requerido")
-        Long idCuentaOrigen,
+        String cuentaDestino,
 
         @Positive(message = "El monto de pago debe ser positivo")
-        @DecimalMin(value = "15.0", inclusive = true, message = "El monto minimo que puede ser transferido es de 15.0 DOP")
+        @DecimalMin(value = "15.00", inclusive = true, message = "El monto minimo que puede ser transferido es de 15.0 DOP")
+       @NotNull(message = "El monto del pago es un campo obligatorio.")
         BigDecimal montoPago
 ) {
 }
