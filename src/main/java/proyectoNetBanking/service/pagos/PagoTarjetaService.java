c4e9b28aea5b4ba368efaf259ef2669df972b478
OpiumnJei <98902680+OpiumnJei@users.noreply.github.com>
2025-03-08 21:33:20 -0400

    retorno de la respuesta que se enviara al usuario a la hora de realizar un pago exitoso, y algunas correciones logicas

--- src/main/java/proyectoNetBanking/service/pagos/PagoTarjetaService.java
+++ src/main/java/proyectoNetBanking/service/pagos/PagoTarjetaService.java
@@ -4,16 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import proyectoNetBanking.domain.cuentasAhorro.CuentaAhorro;
-import proyectoNetBanking.repository.CuentaAhorroRepository;
-import proyectoNetBanking.dto.pagos.DatosPagoTarjetaDTO;
 import proyectoNetBanking.domain.tarjetasCredito.TarjetaCredito;
-import proyectoNetBanking.repository.TarjetaRepository;
 import proyectoNetBanking.domain.transacciones.TipoTransaccion;
 import proyectoNetBanking.domain.transacciones.Transaccion;
-import proyectoNetBanking.service.transacciones.TransaccionService;
+import proyectoNetBanking.dto.pagos.DatosPagoTarjetaDTO;
+import proyectoNetBanking.dto.pagos.ResponsePagoTarjetaDTO;
 import proyectoNetBanking.infra.errors.CuentaNotFoundException;
 import proyectoNetBanking.infra.errors.SaldoInsuficienteException;
 import proyectoNetBanking.infra.errors.TarjetaNotFoundException;
+import proyectoNetBanking.infra.errors.TarjetaSinSaldoPendienteException;
+import proyectoNetBanking.repository.CuentaAhorroRepository;
+import proyectoNetBanking.repository.TarjetaRepository;
+import proyectoNetBanking.service.transacciones.TransaccionService;
 
 import java.math.BigDecimal;
 
@@ -29,14 +31,16 @@ public class PagoTarjetaService {
     @Autowired
     private TransaccionService transaccionService;
 
-
-
     //pago de tarjeta de credito
     @Transactional
-    public Transaccion realizarPagoTarjetaCredito(Long idTarjeta, DatosPagoTarjetaDTO datosPagoTarjetaDTO) {
+    public ResponsePagoTarjetaDTO realizarPagoTarjetaCredito(Long tarjetaId, DatosPagoTarjetaDTO datosPagoTarjetaDTO) {
 
         //verificar que la tarjeta de credito exista
-        TarjetaCredito tarjetaCredito = obtenerTarjetaCredito(idTarjeta);
+        TarjetaCredito tarjetaCredito = obtenerTarjetaCredito(tarjetaId);
+
+        if(tarjetaCredito.getSaldoPorPagar().compareTo(BigDecimal.ZERO) == 0){
+            throw new TarjetaSinSaldoPendienteException("La tarjeta no tiene saldo pendiente por pagar.");
+        }
 
         CuentaAhorro cuentaAhorro = obtenerCuentaAhorroUsuario(datosPagoTarjetaDTO.cuentaId());
 
@@ -45,13 +49,14 @@ public class PagoTarjetaService {
         validarSaldoDisponible(cuentaAhorro, montoPago); //validar que el  monto este disponible en la cuenta de ahorro
 
         //igualar el monto a pagar con el monto de la deuda
-        ajustarMontoPago(tarjetaCredito, montoPago);
+        BigDecimal montoPagoAjustado = ajustarMontoPago(tarjetaCredito, montoPago);
 
         //registar el pago
-        registrarPagoTarjeta(tarjetaCredito, cuentaAhorro, montoPago);
+        registrarPagoTarjeta(tarjetaCredito, cuentaAhorro, montoPagoAjustado);
 
+        BigDecimal saldoPorPagar = tarjetaCredito.getSaldoPorPagar();
         //registrar transaccion
-        return transaccionService.registrarTransaccion(
+        Transaccion transaccion = transaccionService.registrarTransaccion(
                 TipoTransaccion.PAGO_TARJETA,
                 cuentaAhorro,
                 null,
@@ -59,6 +64,16 @@ public class PagoTarjetaService {
                 null,
                 montoPago,
                 "Se realizo un pago a la tarjeta de credito"
+        );
+
+        return new ResponsePagoTarjetaDTO(
+                transaccion.getId(),
+                transaccion.getTipoTransaccion(),
+                transaccion.getFecha(),
+                transaccion.getCuentaOrigen().getId(),
+                transaccion.getTarjetaCredito().getId(),
+                saldoPorPagar,
+                "El pago a la tarjeta se realizó correctamente"
         );
     }
 
@@ -77,27 +92,27 @@ public class PagoTarjetaService {
     }
 
     //metodo gestiona la igualdad entre el monto pagado y el monto a pagar
-    private void ajustarMontoPago(TarjetaCredito tarjetaCredito, BigDecimal montoPago) {
+    private BigDecimal ajustarMontoPago(TarjetaCredito tarjetaCredito, BigDecimal montoPago) {
 
         BigDecimal saldoPorPagar = tarjetaCredito.getSaldoPorPagar(); //saldo a pagar
 
         if (montoPago.compareTo(saldoPorPagar) > 0) {//si la cantidad a pagar es mayor que el saldo a pagar
             //se paga lo que se debe lo demás no se usa
-            montoPago = saldoPorPagar; //la cantidad a pagar sera igual al saldo a pagar
-
+            montoPago = saldoPorPagar; //la cantidad a pagar sera igual al saldo a paga
         }
+
+        return montoPago;
     }
 
-    private CuentaAhorro obtenerCuentaAhorroUsuario(Long idCuenta) {
-        return cuentaAhorroRepository.findById(idCuenta)
+    private CuentaAhorro obtenerCuentaAhorroUsuario(Long cuentaId) {
+        return cuentaAhorroRepository.findById(cuentaId)
                 .orElseThrow(() -> new CuentaNotFoundException("La cuenta no ha sido encontrada."));
     }
 
-    private TarjetaCredito obtenerTarjetaCredito(Long idTarjeta) {
-        return tarjetaRepository.findById(idTarjeta)
+    private TarjetaCredito obtenerTarjetaCredito(Long tarjetaId) {
+        return tarjetaRepository.findById(tarjetaId)
                 .orElseThrow(() -> new TarjetaNotFoundException("Tarjeta de credito no encontrada"));
     }
-
 
     //verificar que Si el saldo disponible en la cuenta es menor al monto enviado por usuario
     private void validarSaldoDisponible(CuentaAhorro cuentaAhorro, BigDecimal montoPago) {
