f8b68a6e1ecf7eeb33de025d42359b91483856f0
OpiumnJei <98902680+OpiumnJei@users.noreply.github.com>
2025-03-09 11:08:37 -0400

    endpoints para realizar el pago de prestamos y pago a beneficiarios agregado

--- src/main/java/proyectoNetBanking/controller/PagosController.java
+++ src/main/java/proyectoNetBanking/controller/PagosController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import proyectoNetBanking.dto.pagos.DatosPagoExpresoDTO;
-import proyectoNetBanking.dto.pagos.DatosPagoTarjetaDTO;
-import proyectoNetBanking.dto.pagos.ResponsePagoExpresoDTO;
-import proyectoNetBanking.dto.pagos.ResponsePagoTarjetaDTO;
+import proyectoNetBanking.dto.pagos.*;
 import proyectoNetBanking.service.pagos.PagoBeneficiarioService;
 import proyectoNetBanking.service.pagos.PagoExpresoService;
 import proyectoNetBanking.service.pagos.PagoPrestamoService;
@@ -28,7 +25,7 @@ public class PagosController {
     private PagoPrestamoService pagoPrestamoService;
 
     @Autowired
-    private PagoBeneficiarioService PagoBeneficiarioService;
+    private PagoBeneficiarioService pagoBeneficiarioService;
 
     @PostMapping("/realizar-pago-expreso")
     public ResponseEntity<ResponsePagoExpresoDTO> realizarPagoExpreso(@RequestBody @Valid DatosPagoExpresoDTO datosPagoExpresoDTO) {
@@ -40,6 +37,18 @@ public class PagosController {
     public ResponseEntity<ResponsePagoTarjetaDTO> realizarPagoTarjeta(@PathVariable Long tarjetaId, @RequestBody @Valid DatosPagoTarjetaDTO datosPagoTarjetaDTO) {
         ResponsePagoTarjetaDTO nuevoPagoTarjeta = pagoTarjetaService.realizarPagoTarjetaCredito(tarjetaId,datosPagoTarjetaDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(nuevoPagoTarjeta);
+    }
+
+    @PostMapping("/realizar-pago-prestamo/{prestamoId}")
+    public ResponseEntity<ResponsePagoPrestamoDTO> realizarPagoPrestamo(@PathVariable Long prestamoId, @RequestBody @Valid DatosPagoPrestamoDTO DatosPagoPrestamoDTO) {
+        ResponsePagoPrestamoDTO nuevoPagoPrestamo = pagoPrestamoService.realizarPagoPrestamo(prestamoId,DatosPagoPrestamoDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoPagoPrestamo);
+    }
+
+    @PostMapping("/realizar-pago-beneficiario/{beneficiarioId}")
+    public ResponseEntity<ResponsePagoBeneficiarioDTO> realizarPago(@PathVariable Long beneficiarioId, @RequestBody @Valid DatosPagoBeneficiarioDTO datosPagoBeneficiarioDTO) {
+        ResponsePagoBeneficiarioDTO nuevoPagoBeneficiario = pagoBeneficiarioService.realizarPagoBeneficiario(beneficiarioId,datosPagoBeneficiarioDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoPagoBeneficiario);
     }
 
 }
