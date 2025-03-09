21a2c1a9fee5af7e240d10a9544df88741e3c4fd
OpiumnJei <98902680+OpiumnJei@users.noreply.github.com>
2025-03-08 21:28:21 -0400

    campo tipoTransaccion agregado

--- src/main/java/proyectoNetBanking/dto/pagos/ResponsePagoExpresoDTO.java
+++ src/main/java/proyectoNetBanking/dto/pagos/ResponsePagoExpresoDTO.java
@@ -1,10 +1,13 @@
 package proyectoNetBanking.dto.pagos;
 
+import proyectoNetBanking.domain.transacciones.TipoTransaccion;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record ResponsePagoExpresoDTO(
         Long transaccionId,
+        TipoTransaccion tipoTransaccion,
         LocalDateTime fechaTransaccion,
         Long cuentaOrigenId,
         String cuentaDestino,
