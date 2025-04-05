78e93c99186a632f0003f299e7e7995a9c5e427e
OpiumnJei <98902680+OpiumnJei@users.noreply.github.com>
2025-04-05 10:52:10 -0400

    metodo findByCedula agregado, se usa para encontrar la cedula del usuario que se esta autenticando.

--- src/main/java/proyectoNetBanking/repository/UsuarioRepository.java
+++ src/main/java/proyectoNetBanking/repository/UsuarioRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import proyectoNetBanking.domain.usuarios.TipoUsuario;
 import proyectoNetBanking.domain.usuarios.Usuario;
 
+import java.util.Optional;
+
 // Usuario Este es el tipo de entidad para la cual el repositorio está diseñado.
 // Loong Este es el tipo de dato de la clave primaria (ID) de la entidad Usuario.
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     boolean existsByCedula(@NotBlank String cedula);
+
+    Optional<Usuario> findByCedula(String numeroCedula); // usado para la autenticacion
 
     boolean existsByCorreo(@NotBlank String correo);
 
