c575a852bafd4351c20fa83657638c5f54dd045b
OpiumnJei <98902680+OpiumnJei@users.noreply.github.com>
2025-07-30 19:42:17 -0400

    Migracion encargada de insertar un usuario del tipo admin

---
+++ src/main/resources/db/migration/V7__insert_admin_user_into_usuarios.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO usuarios (nombre, apellido, cedula, correo, password, tipo_usuario_id, monto_inicial, activo)
+VALUES ('admin', 'admin', '40212393396', 'admin@gmail.com', '$2a$12$Gy8WoyEaSGL9AFylMR7BBeS94n3I.UFHHz2bSGyBYZU4TYh3hJN0W', 1, 0,1);
